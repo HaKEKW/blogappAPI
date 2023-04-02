ccfc986faae0234008989c84feefa0f895020fdc
Hak <art.petr0213@gmail.com>
2023-04-02 14:10:08 +0300

    add permissions

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from posts.models import Post
 from posts.serializers import PostSerializer
+from posts.permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
